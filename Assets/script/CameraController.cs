0649db2206e9f99a23cd616361d19c0bf7c37ef0
dsplayer97 <18813060311@163.com>
2018-06-01 21:58:44 +0800

    射线检测、地图
    射线、地图

---
+++ Assets/script/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float rotateSpeed = 100;       //设置旋转的速度
+    public GameObject PlayerTrans;       //设置空物体的位置
+    public float maxh = 10;               //设置提升的最高高度
+    public GameObject spotlight;   
+    public static GameObject aimGardenfield;
+    private bool aimtofield;
+
+    void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(1))
+        {
+            rotatebasicpanel();
+        }
+        else
+        {
+            raydetect();
+        }
+
+    }
+
+
+    //旋转平面函数
+    private void rotatebasicpanel()
+    {
+        spotlight.SetActive(false);
+        float nor = Input.GetAxis("Mouse X");//获取鼠标的偏移量
+        PlayerTrans.transform.RotateAround(PlayerTrans.transform.position, -Vector3.up, Time.deltaTime * rotateSpeed * nor);//每帧旋转空物体，相机也跟随旋转
+    }
+
+    private void raydetect()
+    {
+        Ray Cameraray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Debug.Log("执行到这了");
+        RaycastHit getGround;
+        if (Physics.Raycast(Cameraray, out getGround))
+        {
+            GameObject gameObject = getGround.transform.gameObject;
+            spotlight.transform.position = new Vector3(gameObject.transform.position.x, spotlight.transform.position.y, gameObject.transform.position.z);
+            spotlight.SetActive(true);
+            int[] info = gameObject.GetComponent<touchtest>().testintarray;
+            aimGardenfield = gameObject;
+
+            Debug.Log(info[0] + "  " + info[1]);
+            
+
+        }
+        else
+        {
+            spotlight.SetActive(false);
+            aimGardenfield = null;
+        }
+    }
+
+   
+
+    
+
+}
---
+++ Assets/script/GardenMap.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GardenMap : MonoBehaviour {
+
+
+    private int[] mapAttribute = new int[49];
+    private int[,] mapstate = new int[7, 7];
+    private static int[,] skinMap = new int[7, 7];
+    private String[] prefabName = { "", "", "","","" };
+
+    private  static GardenMap gardenMap;
+
+    public static GardenMap GetGardenMap()
+    {
+        return gardenMap;
+    }
+
+	// Use this for initialization
+	void Start () {
+        //预设，记得该为调用关卡读取参数
+        int[] attribute = new int[6];
+        //初始化关卡内容参数
+        initmapAttribute(attribute[0], attribute[1], attribute[2], attribute[3], attribute[4], attribute[5]);
+        //初始化地图
+        initmapstate();
+        initskinMap();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+    //初始化随机序列，f1-f5表示五种花的数量
+    private void initmapAttribute(int f1,int f2, int f3, int f4,int f5,int emptyfield)
+    {
+        //按数量存入序列
+        /* 1 - 5表示种子
+         * 7-11表示种好的对应1-5
+         */
+        for(int i = 0; i < f1; i++)
+        {
+            mapAttribute[i] = 1;
+        }
+        for(int i = f1; i < f1 + f2; i++)
+        {
+            mapAttribute[i] = 2;
+        }
+        for (int i = f1+f2; i < f1+ f2+f3; i++)
+        {
+            mapAttribute[i] = 3;
+        }
+        for (int i = f1+ f2+f3; i < f1 + f2 + f3 + f4; i++)
+        {
+            mapAttribute[i] = 4;
+        }
+        for (int i = f1 + f2 + f3 + f4; i < f1 + f2 + f3 + f4 + f5; i++)
+        {
+            mapAttribute[i] = 5;
+        }
+        //存入空地数量
+        for (int i = f1 + f2 + f3 + f4 + f5; i < f1 + f2 + f3 + f4 + f5 + emptyfield; i++)
+        {
+            mapAttribute[i] = 6;
+        }
+        //剩余处放入空
+        for (int i = f1 + f2 + f3 + f4 + f5; i < 49; i++)
+        {
+            mapAttribute[i] = 0;
+        }
+
+    }
+    //初始化地图
+    private void initmapstate()
+    {
+        int[] randomarray = GetRandomArray(49, 0, 49);
+        
+        for(int i = 0; i < 7; i++)
+        {
+            for(int j = 0;j < 7; j++)
+            {
+                mapstate[i, j] = mapAttribute[randomarray[i*7+j]];
+            }
+        }
+
+    }
+    //初始化表面地图
+    private void initskinMap()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            for (int j = 0; j < 7; j++)
+            {
+                skinMap[i, j] = 0;//未点开的为0，点开的为1
+            }
+        }
+    }
+
+    //生成不重复随机数
+    public int[] GetRandomArray(int Number, int minNum, int maxNum)
+    {
+        int j;
+        int[] b = new int[Number];
+        System.Random r = new System.Random();
+        for (j = 0; j < Number; j++)
+        {
+            int i = r.Next(minNum, maxNum + 1);
+            int num = 0;
+            for (int k = 0; k < j; k++)
+            {
+                if (b[k] == i)
+                {
+                    num = num + 1;
+                }
+            }
+            if (num == 0)
+            {
+                b[j] = i;
+            }
+            else
+            {
+                j = j - 1;
+            }
+        }
+        return b;
+    }
+    //加载预载到场景
+    public void loadprefab(int prefabType,Vector3 position) 
+    {
+        UnityEngine.Object prefabobject = Resources.Load(prefabName[prefabType], typeof(GameObject));
+        //用加载得到的资源对象，实例化游戏对象，实现游戏物体的动态加载
+        GameObject prefabGameobject = Instantiate(prefabobject) as GameObject;
+        GameObject parentObject = CameraController.aimGardenfield;
+        prefabGameobject.transform.parent = parentObject.transform;
+        prefabGameobject.transform.localScale = Vector2.one;
+        prefabGameobject.transform.localPosition = Vector3.zero;
+
+    }
+
+
+
+}
