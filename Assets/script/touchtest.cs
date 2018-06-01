faaee3a0815f41240ec126b8535736d361bac0ed
dsplayer97 <18813060311@163.com>
2018-06-01 17:29:55 +0800

    repush
    repush

---
+++ Assets/script/touchtest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touchtest : MonoBehaviour {
+
+
+    public int[] testintarray = new int[2];
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+		
+	}
+}
